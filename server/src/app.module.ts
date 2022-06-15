1e85a441ed4a569031a1a106d6d18107692e1f6d
ekas <ekaspreet93.singh@gmail.com>
2022-06-15 11:00:01 +0200

    feat: Add config for .env file

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { databaseOrmConfig } from 'databaseOrmConfig';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(databaseOrmConfig())],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['../.env'],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(databaseOrmConfig()),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
