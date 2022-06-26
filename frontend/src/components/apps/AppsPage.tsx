2b9a5a1fb819b9eba4c237045b9827304baf919b
ekas <ekaspreet93.singh@gmail.com>
2022-06-26 17:17:05 +0200

    feat: Add avatar with user dropdown

--- frontend/src/components/apps/AppsPage.tsx
+++ frontend/src/components/apps/AppsPage.tsx
@@ -1,18 +1,16 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import { FC } from "react";
-import { Menu } from "antd";
-import {
-  MailOutlined,
-  AppstoreOutlined,
-  SettingOutlined,
-} from "@ant-design/icons";
-import "./index.less";
+import { Avatar, Dropdown, Menu, Space } from "antd";
+import { UserOutlined, CaretDownOutlined } from "@ant-design/icons";
 import logo from "../../assets/logo.svg";
 import appsIcon from "../../assets/apps.svg";
 import dataSourcesIcon from "../../assets/datasource.svg";
+import "./index.less";
+import { Link } from "react-router-dom";
 
 export const AppsPages: FC = () => {
   return (
-    <>
+    <div className="navBarContainer">
       <Menu
         mode="horizontal"
         defaultSelectedKeys={["apps"]}
@@ -38,6 +36,35 @@ export const AppsPages: FC = () => {
           Data Sources
         </Menu.Item>
       </Menu>
-    </>
+      <Dropdown
+        overlay={userMenu}
+        className="menuAvatar"
+        arrow
+        placement="bottomRight"
+      >
+        <a onClick={(e) => e.preventDefault()}>
+          <Space>
+            <Avatar size={35} icon={<UserOutlined />} />
+            <CaretDownOutlined />
+          </Space>
+        </a>
+      </Dropdown>
+    </div>
   );
 };
+
+const userMenu = (
+  <Menu
+    items={[
+      {
+        key: "1",
+        label: <Link to="/">Profile</Link>,
+      },
+      {
+        key: "2",
+        danger: true,
+        label: <Link to="/">Logout</Link>,
+      },
+    ]}
+  />
+);
