53097763c6de6c11e9fec30db3c104f3b9adfb86
Ekas Preet Singh <ekas@27pilots.com>
2022-09-23 20:44:26 +0200

    update: Update DB Schema using prisma ORM

--- backend2/prisma/seed.ts
+++ backend2/prisma/seed.ts
@@ -3,52 +3,208 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
+  await prisma.comment.deleteMany();
+  await prisma.componentData.deleteMany();
+  await prisma.appComponent.deleteMany();
+  await prisma.app.deleteMany();
   await prisma.user.deleteMany();
-  await prisma.post.deleteMany();
 
   console.log('Seeding...');
 
   const user1 = await prisma.user.create({
     data: {
-      email: 'lisa@simpson.com',
-      firstname: 'Lisa',
-      lastname: 'Simpson',
-      password: '$2b$10$EpRnTzVlqHNP0.fUbXUwSOyuiXe/QLSUG6xNekdHgTGmrpHEfIoxm', // secret42
-      role: 'USER',
-      posts: {
-        create: {
-          title: 'Join us for Prisma Day 2019 in Berlin',
-          content: 'https://www.prisma.io/day/',
-          published: true,
+      email: 'john.doe@gmail.com',
+      firstName: 'John',
+      lastName: 'Doe',
+      password: '$2a$10$U5q0hL4C7gAiHoVKESXwQeGFT7LHj56JIzDJrlTp2JKLeJEv27z.S', // secretJohn32
+      role: 'ADMIN',
+    },
+  });
+
+  const app1 = await prisma.app.create({
+    data: {
+      name: 'App1',
+      isPublic: true,
+      slug: 'app1',
+      user: {
+        connect: {
+          id: user1.id,
         },
       },
     },
   });
-  const user2 = await prisma.user.create({
+
+  const appComponent1 = await prisma.appComponent.create({
     data: {
-      email: 'bart@simpson.com',
-      firstname: 'Bart',
-      lastname: 'Simpson',
-      role: 'ADMIN',
-      password: '$2b$10$EpRnTzVlqHNP0.fUbXUwSOyuiXe/QLSUG6xNekdHgTGmrpHEfIoxm', // secret42
-      posts: {
-        create: [
-          {
-            title: 'Subscribe to GraphQL Weekly for community news',
-            content: 'https://graphqlweekly.com/',
-            published: true,
-          },
-          {
-            title: 'Follow Prisma on Twitter',
-            content: 'https://twitter.com/prisma',
-            published: false,
-          },
-        ],
+      name: 'AppComponent1',
+      x: 0,
+      y: 0,
+      width: 100,
+      height: 100,
+      app: {
+        connect: {
+          id: app1.id,
+        },
       },
     },
   });
 
-  console.log({ user1, user2 });
+  const componentData1 = await prisma.componentData.create({
+    data: {
+      data: {},
+      appComponent: {
+        connect: {
+          id: appComponent1.id,
+        },
+      },
+    },
+  });
+
+  const comment1 = await prisma.comment.create({
+    data: {
+      text: 'Comment1 from User John on App 1',
+      app: {
+        connect: {
+          id: app1.id,
+        },
+      },
+      user: {
+        connect: {
+          id: user1.id,
+        },
+      },
+    },
+  });
+
+  const user2 = await prisma.user.create({
+    data: {
+      email: 'bile.simpson@gmail.com',
+      firstName: 'Bile',
+      lastName: 'Simpson',
+      role: 'DEVELOPER',
+      password: '$2a$10$W3linkpv.XJciVgLFc7RfObOtZ23f/63KNTy/5hEMB7BHrOmoOgm2', // secretBile32
+    },
+  });
+
+  const app2 = await prisma.app.create({
+    data: {
+      name: 'App2',
+      isPublic: true,
+      slug: 'app2',
+      user: {
+        connect: {
+          id: user2.id,
+        },
+      },
+    },
+  });
+
+  const appComponent2 = await prisma.appComponent.create({
+    data: {
+      name: 'AppComponent2',
+      x: 0,
+      y: 0,
+      width: 100,
+      height: 100,
+      app: {
+        connect: {
+          id: app2.id,
+        },
+      },
+    },
+  });
+
+  const componentData2 = await prisma.componentData.create({
+    data: {
+      data: {},
+      appComponent: {
+        connect: {
+          id: appComponent2.id,
+        },
+      },
+    },
+  });
+
+  const comment2 = await prisma.comment.create({
+    data: {
+      text: 'Comment2 from User Bile on App 2',
+      app: {
+        connect: {
+          id: app2.id,
+        },
+      },
+      user: {
+        connect: {
+          id: user2.id,
+        },
+      },
+    },
+  });
+
+  const user3 = await prisma.user.create({
+    data: {
+      email: 'ekaspreet93.singh@gmail.com',
+      firstName: 'Ekas Preet',
+      lastName: 'Singh',
+      password: '$2a$10$AX1KUby95KVvgWsgFXrj7e33Pct4yjqrzNwNTO2Yh8eXbrWR6eutO', // secretEkas42
+      role: 'OPERATOR',
+    },
+  });
+
+  const app3 = await prisma.app.create({
+    data: {
+      name: 'App3',
+      isPublic: true,
+      slug: 'app3',
+      user: {
+        connect: {
+          id: user3.id,
+        },
+      },
+    },
+  });
+
+  const appComponent3 = await prisma.appComponent.create({
+    data: {
+      name: 'AppComponent3',
+      x: 0,
+      y: 0,
+      width: 100,
+      height: 100,
+      app: {
+        connect: {
+          id: app3.id,
+        },
+      },
+    },
+  });
+
+  const componentData3 = await prisma.componentData.create({
+    data: {
+      data: {},
+      appComponent: {
+        connect: {
+          id: appComponent3.id,
+        },
+      },
+    },
+  });
+
+  const comment3 = await prisma.comment.create({
+    data: {
+      text: 'Comment3 from User Ekas Preet on App 3',
+      app: {
+        connect: {
+          id: app3.id,
+        },
+      },
+      user: {
+        connect: {
+          id: user3.id,
+        },
+      },
+    },
+  });
 }
 
 main()
